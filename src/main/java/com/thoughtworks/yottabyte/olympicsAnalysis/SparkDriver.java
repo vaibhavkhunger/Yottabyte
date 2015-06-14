0d5a92c3dfb349894df20fe6b5352101df51473b
Rahul Kavale <kavale.rahul@gmail.com>
2015-06-14 14:53:44 +0530

    Rahul Kavale - added sample word count in JAVA 7 api for spark

--- src/main/java/com/thoughtworks/yottabyte/olympicsAnalysis/SparkDriver.java
+++ src/main/java/com/thoughtworks/yottabyte/olympicsAnalysis/SparkDriver.java
@@ -1,17 +1,49 @@
 package com.thoughtworks.yottabyte.olympicsAnalysis;
 
-import org.apache.spark.SparkContext;
-import org.apache.spark.rdd.RDD;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
 
 public class SparkDriver {
     public static void main(String[] args) {
         String master = args[0];
         String appName = args[1];
         String path = args[2];
+        String destination = args[3];
 
-        SparkContext sparkContext = new SparkContext(master, appName);
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
-        RDD<String> lines = sparkContext.textFile(path, 2);
+
+        JavaRDD<String> lines = sc.textFile(path);
+
+        JavaRDD<Tuple2<String, Integer>> linesMappedToOne = lines.map(new Function<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> call(String v1) throws Exception {
+                return new Tuple2(v1, 1);
+            }
+        });
+
+        JavaPairRDD<String, Integer> stringIntegerJavaPairRDD = lines.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<>(s, 1);
+            }
+        });
+
+        JavaPairRDD<String, Integer> wordCounts = stringIntegerJavaPairRDD.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        wordCounts.saveAsTextFile(destination);
 
         System.out.println(lines.first());
     }
