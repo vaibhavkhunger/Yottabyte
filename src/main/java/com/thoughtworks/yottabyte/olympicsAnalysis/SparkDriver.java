79f7c052da3afb2a5c486256580c6e1ad3b1c8a4
Rahul Kavale <kavale.rahul@gmail.com>
2015-06-14 16:17:39 +0530

    Rahul Kavale - added a flatMap function in example

--- src/main/java/com/thoughtworks/yottabyte/olympicsAnalysis/SparkDriver.java
+++ src/main/java/com/thoughtworks/yottabyte/olympicsAnalysis/SparkDriver.java
@@ -4,10 +4,13 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
+
+import java.util.Arrays;
 
 public class SparkDriver {
     public static void main(String[] args) {
@@ -22,14 +25,14 @@ public class SparkDriver {
 
         JavaRDD<String> lines = sc.textFile(path);
 
-        JavaRDD<Tuple2<String, Integer>> linesMappedToOne = lines.map(new Function<String, Tuple2<String, Integer>>() {
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
             @Override
-            public Tuple2<String, Integer> call(String v1) throws Exception {
-                return new Tuple2(v1, 1);
+            public Iterable<String> call(String s) throws Exception {
+                return Arrays.asList(s.split(" "));
             }
         });
 
-        JavaPairRDD<String, Integer> stringIntegerJavaPairRDD = lines.mapToPair(new PairFunction<String, String, Integer>() {
+        JavaPairRDD<String, Integer> stringIntegerJavaPairRDD = words.mapToPair(new PairFunction<String, String, Integer>() {
             @Override
             public Tuple2<String, Integer> call(String s) throws Exception {
                 return new Tuple2<>(s, 1);
