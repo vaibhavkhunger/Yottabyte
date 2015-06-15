7ccf8eae7c0e38fbd5582714b16ca36aec0c3c3a
Dhruv Kapur <dhruvk@thoughtworks.com>
2015-06-15 07:14:54 +0530

    Add naive partitioner implementation for the first problem.

--- src/main/java/com/thoughtworks/yottabyte/vehiclecount/Partitioner.java
+++ src/main/java/com/thoughtworks/yottabyte/vehiclecount/Partitioner.java
@@ -5,7 +5,13 @@ import org.apache.hadoop.io.Text;
 public class Partitioner extends org.apache.hadoop.mapreduce.Partitioner<Text, Text> {
     @Override
     public int getPartition(Text key, Text value, int numPartitions) {
-        return 0;
+        switch (key.toString().toUpperCase()){
+            case "CAR" : return 0;
+            case "TRUCK" : return 1;
+            case "RIKSHAW" : return 2;
+            case "BUS" : return 3;
+            default: return 0;
+        }
     }
 }
 
